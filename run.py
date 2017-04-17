17696d7537d5b7e68a8e2d1b8b32a5341b91512a
Aaron McKinstry <am7057@login-1-1.local>
2017-04-17 17:14:00 -0400

    Added (incomplete, possibly-not-working) script to load the twitter data from hadoop and run a mapreduce job on it.

---
+++ run.py
@@ -0,0 +1,35 @@
+import datetime
+import logging
+
+import pyspark
+import sqlite3
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.FileHandler('/home/am7057/big-data-science-project/script_log.txt'))
+logger.setLevel(logging.INFO)
+
+def get_grid_blocks(south_west_coordinates, north_east_coordinates, n):
+    south_west_coordinates = [float(x) for x in south_west_coordinates]
+    north_east_coordinates = [float(x) for x in north_east_coordinates]
+    latitude_boundaries = [south_west_coordinates[0], north_east_coordinates[0]]
+    latitude_range = latitude_boundaries[1] - latitude_boundaries[0]
+    longitude_boundaries = [south_west_coordinates[1], north_east_coordinates[1]]
+    longitude_range = longitude_boundaries[1] - longitude_boundaries[0]
+    for i in range(1, n):
+        new_latitude_boundary = latitude_boundaries[0] + latitude_range/n
+        latitude_boundaries.insert(-1, new_latitude_boundary)
+        new_longitude_boundary = longitude_boundaries[0] + longitude_range/n
+        longitude_boundaries.insert(-1, new_longitude_boundary)
+    
+    
+
+logger.info(str(datetime.datetime.now()))
+
+logger.info('Processing tweets...')
+
+spark_context = pyspark.SparkContext()
+tweets_csv = spark_context.textFile('tweets.csv').map(
+    lambda x: x)
+logger.info('first tweet entry: ' + str(tweets_csv.first()))
+
+logger.info("finished")
