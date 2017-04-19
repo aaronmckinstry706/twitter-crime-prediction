2e1ad3b93280ecbed100e6d97db7ad399bc676f5
Aaron <aaronmckinstry706@gmail.com>
2017-04-19 14:02:20 -0400

    Finished grid_block_boundaries and associated unit test.

--- run.py
+++ run.py
@@ -4,23 +4,26 @@ import logging
 import pyspark
 import sqlite3
 
-def get_grid_block_boundaries(south_west_coordinates, north_east_coordinates, n):
+def get_grid_block_boundaries(south_west_coordinates, north_east_coordinates,
+                              n):
     south_west_coordinates = [float(x) for x in south_west_coordinates]
     north_east_coordinates = [float(x) for x in north_east_coordinates]
     latitude_boundaries = [south_west_coordinates[0], north_east_coordinates[0]]
     latitude_range = latitude_boundaries[1] - latitude_boundaries[0]
-    longitude_boundaries = [south_west_coordinates[1], north_east_coordinates[1]]
+    longitude_boundaries = [south_west_coordinates[1],
+                            north_east_coordinates[1]]
     longitude_range = longitude_boundaries[1] - longitude_boundaries[0]
     for i in range(1, n):
-        new_latitude_boundary = latitude_boundaries[0] + latitude_range/n
+        new_latitude_boundary = latitude_boundaries[0] + i*latitude_range/n
         latitude_boundaries.insert(-1, new_latitude_boundary)
-        new_longitude_boundary = longitude_boundaries[0] + longitude_range/n
+        new_longitude_boundary = longitude_boundaries[0] + i*longitude_range/n
         longitude_boundaries.insert(-1, new_longitude_boundary)
     return latitude_boundaries, longitude_boundaries
 
 if __name__ == '__main__':
     logger = logging.getLogger(__name__)
-    logger.addHandler(logging.FileHandler('/home/am7057/big-data-science-project/script_log.txt'))
+    logger.addHandler(logging.FileHandler(
+        '/home/am7057/big-data-science-project/script_log.txt'))
     logger.setLevel(logging.INFO)
 
     logger.info(str(datetime.datetime.now()))
--- run_test.py
+++ run_test.py
@@ -5,13 +5,15 @@ import run
 class RunTest(unittest.TestCase):
     
     def test_get_grid_block_boundaries(self):
-        lat_bounds, lon_bounds = get_grid_blocks((0, 0), (10, 10), 10)
+        lat_bounds, lon_bounds = run.get_grid_block_boundaries(
+            (0, 0), (10, 10), 10)
         self.assertListEqual([float(i) for i in range(0, 11)],
                              lat_bounds)
         self.assertListEqual([float(i) for i in range(0, 11)],
                              lon_bounds)
 
-
+if __name__ == '__main__':
+    unittest.main()
 
 
 
