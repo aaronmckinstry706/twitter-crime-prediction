b3d3e1286d05289b18e4d825c93e4d7daa97266a
Aaron McKinstry <aaronmckinstry706@gmail.com>
2017-05-21 12:17:52 -0400

    Changed print_grid.py to use get_grid_block_boundaries() correctly, and set grid divisions to 494 grid blocks (latitude) and 49 blocks (longitude)--based on approximating 1000x1000 m grid squares.

--- print_grid.py
+++ print_grid.py
@@ -5,6 +5,6 @@ import utilities
 # North-east coordinates for New York: lat 40.932936, lon -73.689754
 nyc_bounds = {"SW": (40.491577, -74.274776), "NE": (40.932936, -73.689754)}
 lat_grid_bounds, lon_grid_bounds = utilities.get_grid_block_boundaries(
-    nyc_bounds["SW"], nyc_bounds["NE"], 10)
+    nyc_bounds["SW"], nyc_bounds["NE"], 494, 49)
 grid_bounds = utilities.get_grid_square_bounds(lat_grid_bounds, lon_grid_bounds)
 utilities.print_grid_csv('grid_bounds.csv', grid_bounds)
