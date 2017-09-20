2dd3689350c8b14cc05bee0eff917097fcbee6bd
Aaron <aaronmckinstry706@gmail.com>
2017-09-20 15:08:59 -0400

    Added LatLonGrid class & grid.py fxn.

--- grid.py
+++ grid.py
@@ -1,56 +1,54 @@
-import csv
+import bisect
+import collections
 import math
-import os
 
-def get_longitude_delta(meters_delta, current_latitude):
+"""A class which defines a grid square by its upper and lower lat/lon bounds."""
+GridSquare = collections.namedtuple('GridSquare', ['lat_min', 'lat_max', 'lon_min', 'lon_max'])
+
+class LatLonGrid(object):
+    """
+    An object representing a rectangular grid of latitude and longitude regions. The latitude grid
+    boundaries are defined by range(lat_min, lat_max + lat_step, lat_step), and the longitude grid
+    boundaries are defined similarly.
+    """
+    def __init__(self, lat_min, lat_max, lon_min, lon_max, lat_step, lon_step):
+        self._lat_bounds = tuple(range(lat_min, lat_max + lat_step, lat_step))
+        self._lon_bounds = tuple(range(lon_min, lon_max + lon_step, lon_step))
+    
+    def grid_square_index(self, lat, lon):
+        """Given a position defined by (lat, lon), this function returns the index of the grid
+        square in which this position lies. If the position lies outside of the grid, then -1 is
+        returned."""
+        lat_index = bisect.bisect_left(self._lat_bounds, lat)
+        lon_index = bisect.bisect_left(self._lon_bounds, lon)
+        if lat_index == 0 or lat_index == len(self._lat_bounds) \
+                          or lon_index == 0 or lon_index == len(self._lon_bounds):
+            return -1
+        lat_index = lat_index - 1
+        lon_index = lon_index - 1
+        return lat_index * self.lat_grid_dimension + lon_index
+    
+    @property
+    def lat_grid_dimension(self):
+        return len(self._lat_bounds) - 1
+    
+    @property
+    def lon_grid_dimension(self):
+        return len(self._lon_bounds) - 1
+
+def get_longitude_delta(meters_delta, current_lat):
     """At a given latitude, this function calculates how many degrees in longitude
     one would need to change in order to change position by a given number of meters."""
-    # current_latitude should be in degrees, obv
-    earth_radius_meters = 6371008 # https://nssdc.gsfc.nasa.gov/planetary/factsheet/earthfact.html
+    # current_lat should be in degrees, obv
+    earth_radius_meters = 6371008.0 # https://nssdc.gsfc.nasa.gov/planetary/factsheet/earthfact.html
     meters_delta = float(meters_delta)
-    current_latitude = float(current_latitude)
-    return (meters_delta / (earth_radius_meters * math.cos(current_latitude / 180.0 * math.pi))) \
+    current_lat = float(current_lat)
+    return (meters_delta / (earth_radius_meters * math.cos(current_lat / 180.0 * math.pi))) \
         * 180.0 / math.pi
 
-def get_grid_block_boundaries(south_west_coordinates, north_east_coordinates, n_lat, n_lon):
-    # type: (Tuple[float, float], Tuple[float, float]) -> Tuple[List[float], List[float]]
-    south_west_coordinates = [float(x) for x in south_west_coordinates]
-    north_east_coordinates = [float(x) for x in north_east_coordinates]
-    latitude_boundaries = [south_west_coordinates[0], north_east_coordinates[0]]
-    latitude_range = latitude_boundaries[1] - latitude_boundaries[0]
-    longitude_boundaries = [south_west_coordinates[1],
-                            north_east_coordinates[1]]
-    longitude_range = longitude_boundaries[1] - longitude_boundaries[0]
-    for i in range(1, n_lat):
-        new_latitude_boundary = latitude_boundaries[0] + i*latitude_range/n_lat
-        latitude_boundaries.insert(-1, new_latitude_boundary)
-    for i in range(1, n_lon):
-        new_longitude_boundary = longitude_boundaries[0] + i*longitude_range/n_lon
-        longitude_boundaries.insert(-1, new_longitude_boundary)
-    return latitude_boundaries, longitude_boundaries    
-
-def get_grid_square_bounds(latitude_boundaries, longitude_boundaries):
-    # type: (List[float], List[float]) -> List[Tuple[float, float, float, float]]
-    grid_squares = []
-    for i in range(len(latitude_boundaries)-1):
-        for j in range(len(longitude_boundaries)-1):
-            grid_squares.append(
-                (latitude_boundaries[i], latitude_boundaries[i+1],
-                 longitude_boundaries[j], longitude_boundaries[j+1]))
-    return grid_squares
-
-def print_grid_csv(filename, grid_squares):
-    # type: (str, List[Tuple[float, float, float, float]]) -> None
-    with open(filename, 'wb') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        for i in range(len(grid_squares)):
-            csv_writer.writerow((i,) + grid_squares[i])
-
-def read_grid_csv(filename):
-    with open(filename, 'rb') as csv_file:
-        csv_reader = csv.reader(csv_file)
-        grid_squares = []
-        for row in csv_reader:
-            grid_squares.append(tuple(float(num) for num in row[1:]))
-        return grid_squares
-
+def get_latitude_delta(meters_delta):
+    """This function calculates how many degrees in latitude one would need to change in order to
+    change position by a given number of meters."""
+    earth_radius_meters = 6371008.0 # https://nssdc.gsfc.nasa.gov/planetary/factsheet/earthfact.html
+    meters_delta = float(meters_delta)
+    return meters_delta / earth_radius_meters * 180 / math.pi
--- grid_test.py
+++ grid_test.py
@@ -3,29 +3,32 @@ import unittest
 
 import grid
 
+class GridSquareTest(unittest.TestCase):
+    def setUp(self):
+        self.latlongrid = grid.LatLonGrid(
+            lat_min=0,
+            lat_max=10,
+            lat_step=1,
+            lon_min=0,
+            lon_max=11,
+            lon_step=1)
+    
+    def test_grid_dimensions(self):
+        self.assertEqual(10, self.latlongrid.lat_grid_dimension)
+        self.assertEqual(11, self.latlongrid.lon_grid_dimension)
+    
+    def test_grid_square_index(self):
+        self.assertEqual(3*10 + 3, self.latlongrid.grid_square_index(3.5, 3.5))
+        self.assertEqual(-1, self.latlongrid.grid_square_index(-0.5, 3.5))
+        self.assertEqual(-1, self.latlongrid.grid_square_index(9.0, 11.5))
+
 class GridTest(unittest.TestCase):
     
-    def test_get_grid_block_boundaries(self):
-        lat_bounds, lon_bounds = grid.get_grid_block_boundaries(
-            (0, 0), (10, 10), 10, 5)
-        self.assertListEqual([float(i) for i in range(0, 11)],
-                             lat_bounds)
-        self.assertListEqual([float(i)*2 for i in range(0, 6)],
-                             lon_bounds)
-    
-    def test_reading_and_printing_grid_square_csv(self):
-        grid_squares = [(0, 1, 0, 1), (0, 1, 1, 2), (1, 2, 0, 1), (1, 2, 1, 2)]
-        grid.print_grid_csv('temp.csv', grid_squares)
-        self.assertEqual(
-            [(0, 1, 0, 1), (0, 1, 1, 2), (1, 2, 0, 1), (1, 2, 1, 2)],
-            grid.read_grid_csv('temp.csv'))
-        os.remove('temp.csv')
-
     def test_get_longitude_delta(self):
         self.assertAlmostEqual(0.0126829387, grid.get_longitude_delta(1000, 45), places=4)
     
-    def test_get_grid_square_bounds(self):
-        self.assertEqual(
-            [(0, 1, 0, 1), (0, 1, 1, 2), (1, 2, 0, 1), (1, 2, 1, 2)],
-            grid.get_grid_square_bounds(range(3), range(3)))
+    def test_git_latitude_delta(self):
+        self.assertAlmostEqual(0.00899832896, grid.get_latitude_delta(1000), places=4)
 
+if __name__ == '__main__':
+    unittest.main()
