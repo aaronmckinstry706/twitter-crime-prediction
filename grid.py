bca4ae2356bd20df0939f08454392bca704903c5
Aaron <aaronmckinstry706@gmail.com>
2017-09-20 16:37:30 -0400

    Fixed behavior of LatLonGrid.

--- grid.py
+++ grid.py
@@ -2,18 +2,42 @@ import bisect
 import collections
 import math
 
-"""A class which defines a grid square by its upper and lower lat/lon bounds."""
-GridSquare = collections.namedtuple('GridSquare', ['lat_min', 'lat_max', 'lon_min', 'lon_max'])
+"""A class which defines a square by its upper and lower lat/lon bounds."""
+LatLonSquare = collections.namedtuple('Square', ['lat_min', 'lat_max', 'lon_min', 'lon_max'])
 
 class LatLonGrid(object):
     """
-    An object representing a rectangular grid of latitude and longitude regions. The latitude grid
-    boundaries are defined by range(lat_min, lat_max + lat_step, lat_step), and the longitude grid
-    boundaries are defined similarly.
+    An object representing a rectangular grid of latitude and longitude regions. The latitude
+    boundaries are placed at even intervals of lat_step until lat_max is passed or reached. The
+    final latitude boundary is then lat_max. The longitude boundaries are placed similarly. 
+    
+    Edge case, for clarification: LatLonGrid(0, 1, 0.15, ...) will have latitude boundaries of
+    approximately [0, 0.15, 0.3, 0.45, 0.6, 0.75, 0.9, 1.0]. LatLonGrid(0, 1, 0.5) will have
+    latitude boundaries of [0, 0.5, 1.0]. The same holds for longitude boundaries when the
+    longitude max, min and step are all defined as in the two examples given. 
     """
     def __init__(self, lat_min, lat_max, lon_min, lon_max, lat_step, lon_step):
-        self._lat_bounds = tuple(range(lat_min, lat_max + lat_step, lat_step))
-        self._lon_bounds = tuple(range(lon_min, lon_max + lon_step, lon_step))
+        # Make sure all args are floats.
+        lat_min = float(lat_min)
+        lat_max = float(lat_max)
+        lon_min = float(lon_min)
+        lon_max = float(lon_max)
+        lat_step = float(lat_step)
+        lon_step = float(lon_step)
+        
+        num_lat_bounds = (lat_max - lat_min) / lat_step
+        self._lat_bounds = []
+        for b in range(0, int(num_lat_bounds) + 1, 1):
+            self._lat_bounds.append((lat_max - lat_min) * (b / num_lat_bounds) + lat_min)
+        self._lat_bounds.append(lat_max)
+        self._lat_bounds = tuple(self._lat_bounds)
+        
+        num_lon_bounds = (lon_max - lon_min) / lon_step
+        self._lon_bounds = []
+        for b in range(0, int(num_lon_bounds) + 1, 1):
+            self._lon_bounds.append((lon_max - lon_min) * (b / num_lon_bounds) + lon_min)
+        self._lon_bounds.append(lon_max)
+        self._lon_bounds = tuple(self._lon_bounds)
     
     def grid_square_index(self, lat, lon):
         """Given a position defined by (lat, lon), this function returns the index of the grid
@@ -36,7 +60,7 @@ class LatLonGrid(object):
     def lon_grid_dimension(self):
         return len(self._lon_bounds) - 1
 
-def get_longitude_delta(meters_delta, current_lat):
+def get_lon_delta(meters_delta, current_lat):
     """At a given latitude, this function calculates how many degrees in longitude
     one would need to change in order to change position by a given number of meters."""
     # current_lat should be in degrees, obv
@@ -46,7 +70,7 @@ def get_longitude_delta(meters_delta, current_lat):
     return (meters_delta / (earth_radius_meters * math.cos(current_lat / 180.0 * math.pi))) \
         * 180.0 / math.pi
 
-def get_latitude_delta(meters_delta):
+def get_lat_delta(meters_delta):
     """This function calculates how many degrees in latitude one would need to change in order to
     change position by a given number of meters."""
     earth_radius_meters = 6371008.0 # https://nssdc.gsfc.nasa.gov/planetary/factsheet/earthfact.html
--- grid_test.py
+++ grid_test.py
@@ -8,27 +8,28 @@ class GridSquareTest(unittest.TestCase):
         self.latlongrid = grid.LatLonGrid(
             lat_min=0,
             lat_max=10,
-            lat_step=1,
+            lat_step=0.9,
             lon_min=0,
             lon_max=11,
-            lon_step=1)
+            lon_step=0.9)
     
     def test_grid_dimensions(self):
-        self.assertEqual(10, self.latlongrid.lat_grid_dimension)
-        self.assertEqual(11, self.latlongrid.lon_grid_dimension)
+        self.assertEqual(12, self.latlongrid.lat_grid_dimension)
+        self.assertEqual(13, self.latlongrid.lon_grid_dimension)
     
     def test_grid_square_index(self):
-        self.assertEqual(3*10 + 3, self.latlongrid.grid_square_index(3.5, 3.5))
+        self.assertEqual(3*self.latlongrid.lat_grid_dimension + 3,
+                         self.latlongrid.grid_square_index(3.5, 3.5))
         self.assertEqual(-1, self.latlongrid.grid_square_index(-0.5, 3.5))
         self.assertEqual(-1, self.latlongrid.grid_square_index(9.0, 11.5))
 
 class GridTest(unittest.TestCase):
     
     def test_get_longitude_delta(self):
-        self.assertAlmostEqual(0.0126829387, grid.get_longitude_delta(1000, 45), places=4)
+        self.assertAlmostEqual(0.0126829387, grid.get_lon_delta(1000, 45), places=4)
     
     def test_git_latitude_delta(self):
-        self.assertAlmostEqual(0.00899832896, grid.get_latitude_delta(1000), places=4)
+        self.assertAlmostEqual(0.00899832896, grid.get_lat_delta(1000), places=4)
 
 if __name__ == '__main__':
     unittest.main()
