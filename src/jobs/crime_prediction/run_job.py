4a83e29809849694227fa43bfa780a2dd84e6768
Aaron <aaronmckinstry706@gmail.com>
2017-10-06 16:22:10 -0400

    Tested twitter data import.

--- src/jobs/crime_prediction/run_job.py
+++ src/jobs/crime_prediction/run_job.py
@@ -1,3 +1,39 @@
+import pyspark.sql as sql
+import pyspark.sql.types as types
+
+def import_twitter_data(job_context):
+    """Imports the twitter data as DataFrame, drops all columns except
+    [timestamp, lon, lat, tweet], and returns resulting DataFrame."""
+    
+    required_keys = ['tweets_file_path', 'spark_session']
+    if not all([(key in job_context) for key in required_keys]):
+        missing_keys = [k for k in required_keys if k not in job_context]
+        raise ValueError(
+            'job_context {} is missing keys {}.'.format(job_context, missing_keys))
+    
+    tweets_schema = types.StructType([
+      types.StructField('id', types.LongType()),
+      types.StructField('timestamp', types.LongType(), nullable=False),
+      types.StructField('postalCode', types.StringType()),
+      types.StructField('lon', types.DoubleType(), nullable=False),
+      types.StructField('lat', types.DoubleType(), nullable=False),
+      types.StructField('tweet', types.StringType(), nullable=False),
+      types.StructField('user_id', types.LongType()),
+      types.StructField('application', types.StringType()),
+      types.StructField('source', types.StringType())])
+    
+    tweets_df = job_context['spark_session'].read.csv(
+        job_context['tweets_file_path'],
+        escape='"',
+        header='true',
+        schema=tweets_schema,
+        mode='DROPMALFORMED')
+    
+    tweets_df = tweets_df.select(['timestamp', 'lon', 'lat', 'tweet'])
+    return tweets_df
 
 def run(sc, args):
-    print("doing something...")
+    job_context = {}
+    job_context['spark_context'] = sc
+    job_context['spark_session'] = sql.SparkSession(sc)
+    job_context['tweets_file_path'] = 'tweets2.csv'
--- src/jobs/crime_prediction/test_job.py
+++ src/jobs/crime_prediction/test_job.py
@@ -1,7 +1,9 @@
+import subprocess
 import unittest
 
 import pyspark
 import pyspark.sql as sql
+import pyspark.sql.types as types
 
 from jobs.crime_prediction import run_job as run_job
 
@@ -18,3 +20,39 @@ class TestJob(unittest.TestCase):
     def tearDownClass(cls):
         cls.ss.stop()
         cls.sc.stop()
+    
+    def setUp(self):
+        self.job_context = {
+            'spark_context': self.sc,
+            'spark_session': self.ss}
+        
+    def test_import_twitter_data(self):
+        file_name = __name__ + '.csv'
+        self.job_context['tweets_file_path'] = file_name
+        
+        input_df = self.ss.createDataFrame([
+            sql.Row(id=14580, timestamp=8888888, postalCode='95823',
+                    lon=34.256, lat=34.258, tweet='something something',
+                    user_id=44438487, application='instagram', source='instagram?')],
+            schema=types.StructType([
+                types.StructField('id', types.LongType()),
+                types.StructField('timestamp', types.LongType(), nullable=False),
+                types.StructField('postalCode', types.StringType()),
+                types.StructField('lon', types.DoubleType(), nullable=False),
+                types.StructField('lat', types.DoubleType(), nullable=False),
+                types.StructField('tweet', types.StringType(), nullable=False),
+                types.StructField('user_id', types.LongType()),
+                types.StructField('application', types.StringType()),
+                types.StructField('source', types.StringType())]))
+        
+        try:
+            input_df.coalesce(1).write.option('header', 'true').csv(file_name)
+            actual = run_job.import_twitter_data(self.job_context).first().asDict()
+            expected = sql.Row(timestamp=8888888, lon=34.256, lat=34.258,
+                               tweet='something something').asDict()
+            self.assertDictEqual(expected, actual)
+        except:
+            raise
+        finally:
+            subprocess.call(['hadoop', 'fs', '-rm', '-r', '-f', file_name])
+
