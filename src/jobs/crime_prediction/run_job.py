2ff683c5126588921e4389ed1bebc52e21a973ea
Aaron <aaronmckinstry706@gmail.com>
2017-10-08 15:51:16 -0400

    Added filter_by_dates and unit test.

--- src/jobs/crime_prediction/run_job.py
+++ src/jobs/crime_prediction/run_job.py
@@ -1,15 +1,17 @@
+import datetime
+
 import pyspark.sql as sql
+import pyspark.sql.functions as functions
 import pyspark.sql.types as types
 
-def import_twitter_data(job_context):
-    """Imports the twitter data as DataFrame, drops all columns except
-    [timestamp, lon, lat, tweet], and returns resulting DataFrame."""
+def import_twitter_data(spark_session, tweets_file_path):
+    """Imports the twitter data and returns resulting DataFrame, with columns
+    [timestamp, lon, lat, tweet].
     
-    required_keys = ['tweets_file_path', 'spark_session']
-    if not all([(key in job_context) for key in required_keys]):
-        missing_keys = [k for k in required_keys if k not in job_context]
-        raise ValueError(
-            'job_context {} is missing keys {}.'.format(job_context, missing_keys))
+    Args:
+        spark_session    --    An active SparkSession.
+        tweets_file_path    --    A file path.
+    """
     
     tweets_schema = types.StructType([
       types.StructField('id', types.LongType()),
@@ -22,8 +24,8 @@ def import_twitter_data(job_context):
       types.StructField('application', types.StringType()),
       types.StructField('source', types.StringType())])
     
-    tweets_df = job_context['spark_session'].read.csv(
-        job_context['tweets_file_path'],
+    tweets_df = spark_session.read.csv(
+        tweets_file_path,
         escape='"',
         header='true',
         schema=tweets_schema,
@@ -32,8 +34,37 @@ def import_twitter_data(job_context):
     tweets_df = tweets_df.select(['timestamp', 'lon', 'lat', 'tweet'])
     return tweets_df
 
+def filter_by_dates(spark_session, tweets_df, start_date_inclusive, end_date_exclusive):
+    """Converts the 'timestamp' column to 'date' column of type DateTime, then returns a
+    DataFrame containing only tweets which occurred between the start (inclusive) and end
+    (exclusive) dates; columns are ['date', 'lat', 'lon', 'tweet'].
+    
+    Args:
+        spark_session    --    An active SparkSession.
+        tweets_df    --    A DataFrame with a 'timestamp' column of type LongType.
+        start_date_inclusive    --    A datetime.date object.
+        end_date_exclusive    --    A datetime.date object.
+    """
+    
+    def timestamp_to_date(t):
+        # The date is calculated.
+        return datetime.datetime.utcfromtimestamp(t).replace(tzinfo=None).date()
+    timestamp_to_date_udf = functions.udf(
+        timestamp_to_date, returnType=types.DateType())
+    
+    date_column = timestamp_to_date_udf(tweets_df['timestamp'])
+    tweets_df = (tweets_df.withColumn('date', date_column)
+                          .drop('timestamp'))
+    
+    date_to_column = functions.lit(end_date_exclusive)
+    date_from_column = functions.lit(start_date_inclusive)
+    
+    tweets_df = tweets_df.filter(
+        (tweets_df['date'] >= date_from_column)
+        & (tweets_df['date'] < date_to_column))
+    
+    return tweets_df
+
 def run(sc, args):
-    job_context = {}
-    job_context['spark_context'] = sc
-    job_context['spark_session'] = sql.SparkSession(sc)
-    job_context['tweets_file_path'] = 'tweets2.csv'
+    ss = sql.SparkSession(sc)
+
--- src/jobs/crime_prediction/test_job.py
+++ src/jobs/crime_prediction/test_job.py
@@ -1,3 +1,4 @@
+import datetime
 import subprocess
 import unittest
 
@@ -14,6 +15,7 @@ class TestJob(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.sc = pyspark.SparkContext(appName=cls.__class__.__name__)
+        cls.sc.setLogLevel('FATAL')
         cls.ss = sql.SparkSession(cls.sc)
     
     @classmethod
@@ -21,14 +23,9 @@ class TestJob(unittest.TestCase):
         cls.ss.stop()
         cls.sc.stop()
     
-    def setUp(self):
-        self.job_context = {
-            'spark_context': self.sc,
-            'spark_session': self.ss}
-        
+    @unittest.skip("Skipping until run_job is finished.")
     def test_import_twitter_data(self):
         file_name = __name__ + '.csv'
-        self.job_context['tweets_file_path'] = file_name
         
         input_df = self.ss.createDataFrame([
             sql.Row(id=14580, timestamp=8888888, postalCode='95823',
@@ -47,7 +44,7 @@ class TestJob(unittest.TestCase):
         
         try:
             input_df.coalesce(1).write.option('header', 'true').csv(file_name)
-            actual = run_job.import_twitter_data(self.job_context).first().asDict()
+            actual = run_job.import_twitter_data(self.ss, file_name).first().asDict()
             expected = sql.Row(timestamp=8888888, lon=34.256, lat=34.258,
                                tweet='something something').asDict()
             self.assertDictEqual(expected, actual)
@@ -55,4 +52,51 @@ class TestJob(unittest.TestCase):
             raise
         finally:
             subprocess.call(['hadoop', 'fs', '-rm', '-r', '-f', file_name])
+    
+    def test_filter_by_dates(self):
+        input_tweets_schema = types.StructType([
+            types.StructField('timestamp', types.LongType()),
+            types.StructField('lat', types.DoubleType()),
+            types.StructField('lon', types.DoubleType()),
+            types.StructField('tweet', types.StringType())])
+        
+        output_tweets_schema = types.StructType([
+            types.StructField('date', types.DateType()),
+            types.StructField('lat', types.DoubleType()),
+            types.StructField('lon', types.DoubleType()),
+            types.StructField('tweet', types.StringType())])
+        
+        EPOCH = datetime.date(1970, 1, 1)
+        start_date = datetime.date(2016, 3, 3)
+        start_timestamp = int((start_date - EPOCH).total_seconds())
+        end_date = datetime.date(2016, 3, 5)
+        end_timestamp = int((end_date - EPOCH).total_seconds())
+        
+        input_timestamps = [
+            start_timestamp  - 1,
+            start_timestamp,
+            start_timestamp + 1,
+            end_timestamp - 1,
+            end_timestamp,
+            end_timestamp + 24*60*60]
+        
+        input_tweets_df = self.ss.createDataFrame(
+            [sql.Row(timestamp=t, lat=34.89776, lon=48.3847, tweet=str(t))
+             for t in input_timestamps],
+            schema=input_tweets_schema)
+        
+        output_tweets_df = self.ss.createDataFrame(
+            [sql.Row(
+                date=datetime.datetime.utcfromtimestamp(t).replace(tzinfo=None).date(),
+                lat=34.89776, lon=48.3847, tweet=str(t))
+             for t in input_timestamps if t >= start_timestamp and t < end_timestamp],
+            schema=output_tweets_schema)
+        
+        actual_df = run_job.filter_by_dates(
+            self.ss, input_tweets_df, start_date, end_date)
+        actual_list = sorted(actual_df.collect(), key=lambda d: d['tweet'])
+        expected_list = sorted(output_tweets_df.collect(), key=lambda d: d['tweet'])
+        
+        self.assertListEqual([r.asDict() for r in expected_list],
+                             [r.asDict() for r in actual_list])
 
