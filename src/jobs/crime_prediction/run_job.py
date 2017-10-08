65464f79e4a9523d6a0a178a90b06b8136521711
Aaron <aaronmckinstry706@gmail.com>
2017-10-08 19:08:33 -0400

    Generated topic distribution.

--- src/jobs/crime_prediction/run_job.py
+++ src/jobs/crime_prediction/run_job.py
@@ -2,6 +2,9 @@ import argparse
 import datetime
 import operator
 
+import pyspark.ml as ml
+import pyspark.ml.feature as feature
+import pyspark.ml.clustering as clustering
 import pyspark.sql as sql
 import pyspark.sql.functions as functions
 import pyspark.sql.types as types
@@ -39,9 +42,9 @@ def import_twitter_data(spark_session, tweets_file_path):
     return tweets_df
 
 def filter_by_dates(spark_session, tweets_df, start_date_inclusive, end_date_exclusive):
-    """Converts the 'timestamp' column to 'date' column of type DateTime, then returns a
+    """Replaces the 'timestamp' column with 'date' column of type DateTime, then returns a
     DataFrame containing only entries with a 'date' between the start (inclusive) and end
-    (exclusive) dates; columns are ['date', 'lat', 'lon', 'tweet'].
+    (exclusive) dates.
     
     The timestamps, and the resulting dates, are both assumed to be in UTC time. However,
     the resulting dates are naive (i.e., not timezone-aware) datetime.date objects.
@@ -74,8 +77,9 @@ def filter_by_dates(spark_session, tweets_df, start_date_inclusive, end_date_exc
 
 def group_by_grid_square_and_tokenize(spark_session, latlongrid, tweets_df):
     """Calculates the grid square id from 'lat' and 'lon' columns in tweets_df, and then
-    groups the tweets by grid square. Tweets are then tokenized. Returned dataframe has
-    columns ['grid_square', 'tokens'].
+    groups the tweets by grid square. Tweets are tokenized. Returned dataframe has
+    columns ['grid_square', 'tokens'], where 'tokens' is a list of all tokens from every
+    tweet within an entry's 'grid_square'.
     
     Args:
         spark_session    --    An active SparkSession.
@@ -108,19 +112,40 @@ def group_by_grid_square_and_tokenize(spark_session, latlongrid, tweets_df):
 def run(sc, args):
     sc.setLogLevel('FATAL')
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument('year', help='Year of prediction, in format YYYY.')
-    arg_parser.add_argument('month', help='Month of prediction, in format MM')
-    arg_parser.add_argument('day', help='Day of prediction, in format DD.')
-    
-    arg_parser.parse_args(args)
+    arg_parser.add_argument('year', help='Year of prediction, in format YYYY.', type=int)
+    arg_parser.add_argument('month', help='Month of prediction, in format MM.', type=int)
+    arg_parser.add_argument('day', help='Day of prediction, in format DD.', type=int)
+    args = arg_parser.parse_args(args)
     
     ss = sql.SparkSession(sc)
     
+    latlongrid = grid.LatLonGrid(
+        lat_min=40.488320,
+        lat_max=40.957189,
+        lon_min=-74.290739,
+        lon_max=-73.635679,
+        lat_step=grid.get_lon_delta(1000, (40.957189 - 40.488320)/2.0),
+        lon_step=grid.get_lat_delta(1000))
+    
     tweets_df = import_twitter_data(ss, 'tweets2.csv')
     
-    prediction_date = datetime.date(arg_parser.year, arg_parser.month, arg_parser.day)
+    prediction_date = datetime.date(args.year, args.month, args.day)
     NUM_DAYS_IN_HISTORY = 31
     history_cutoff = prediction_date - datetime.timedelta(days=NUM_DAYS_IN_HISTORY)
     filtered_tweets_df = filter_by_dates(ss, tweets_df, history_cutoff, prediction_date)
+    
     tokens_df = group_by_grid_square_and_tokenize(ss, latlongrid, filtered_tweets_df)
+    
+    hashing_tf = feature.HashingTF(
+        numFeatures=(2^18)-1, inputCol='tokens', outputCol='token_frequencies')
+    lda = (clustering.LDA()
+        .setFeaturesCol('token_frequencies')
+        .setK(10)
+        .setTopicDistributionCol('topic_distribution'))
+    topic_distribution_pipeline = ml.Pipeline(stages=[hashing_tf, lda])
+    lda_model = topic_distribution_pipeline.fit(tokens_df)
+    topic_distributions = (lda_model.transform(tokens_df)
+                           .select(['grid_square', 'topic_distribution']))
+    
+    topic_distributions.show()
 
